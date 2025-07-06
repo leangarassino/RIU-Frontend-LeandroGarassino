2576f6cb5571e95012f44d5ba87f5a3eda5650a9
leangarassino <leandro.garassino@gmail.com>
2025-07-06 00:07:45 -0300

    chore: initial configs in the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Welcome } from './pages/welcome/welcome';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'welcome',
+    component: Welcome,
+  },
+  {
+    path: '',
+    redirectTo: 'welcome',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'welcome',
+  },
+];
